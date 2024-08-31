d1fc4cedb63014513a61ae8c7b9dc80041169b28
J M <jm@Js-MacBook-Pro.hsd1.fl.comcast.net>
2024-08-31 10:13:25 -0400

    added screen size adaptability to about

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,25 +1,11 @@
-
+import styles from "../css/about.module.css";
 
 export default function About() {
   return (
     <>
-      <div style={{
-        display: 'flex'
-      }}>
-        <div
-          style={{
-            margin: "10px",
-            width: "90%",
-            height: "auto",
-            display: "flex",
-            justifyContent: "space-evenly",
-          }}
-        >
-          <div
-            class="card bg-primary-subtle"
-            align="center"
-            style={{ marginTop: "10px", width: "70%", height: "auto" }}
-          >
+      <div>
+        <div>
+          <div className={`${styles.section} card bg-primary-subtle`} align="center">
             <h1>
               <strong>About Me</strong>
             </h1>
@@ -36,82 +22,52 @@ export default function About() {
         </div>
       </div>
 
-      <div>
-        <div
-          style={{
-            margin: "10px",
-            width: window.innerWidth > 768 ? "90%" : "100%",
-            height: "auto",
-            display: "flex",
-            justifyContent: "space-evenly",
-          }}
-        >
-          <div style={{ display: "flex", width: "70%" }}>
-            <div>
-              <div
-                class="card bg-primary-subtle"
-                align="center"
-                style={{ marginTop: "10px", width: "90%", height: "auto" }}
-              >
-                <h1>
-                  <strong>Psychology Background</strong>
-                </h1>
-                <p>
-                  The "Journey of Life" can be seen as a path we all walk,
-                  filled with different experiences and challenges. Just like
-                  moving through various landscapes—a desert, an ocean, or a
-                  dense jungle—each part of life brings its own difficulties and
-                  rewards.
-                </p>
-                <p>
-                  At times, we might feel lost or uncertain, like wandering
-                  through a desert. Other times, we might feel overwhelmed, like
-                  being caught in stormy seas. We face obstacles, make mistakes,
-                  and sometimes get stuck in difficult situations. But each
-                  experience, whether easy or tough, helps us learn and grow.
-                </p>
-                <p>
-                  Ultimately, life is about finding our way, learning from our
-                  experiences, and continuing to move forward with hope, no
-                  matter what challenges come our way.
-                </p>
-                <img
-                  src="https://github.com/user-attachments/assets/5a45eb51-330a-43dc-aae5-12c05fe4967e"
-                  alt="The journey of life"
-                />
-              </div>
-            </div>
-            <div>
-              <div
-                style={{
-                  margin: "10px",
-                  width: "90%",
-                  height: "auto",
-                  display: "flex",
-                  justifyContent: "space-evenly",
-                }}
-              >
-                <div
-                  class="card bg-primary-subtle"
-                  align="center"
-                  style={{ marginRight: "15%", height: "auto" }}
-                >
-                  <h1>
-                    <strong>Jose Muchati</strong>
-                  </h1>
-                  <img src="https://avatars.githubusercontent.com/u/133266199?v=4" alt="me" />
-                  <p>
-                    I am driven by a passion for personal growth and continuous
-                    learning, embracing challenges with adaptability and
-                    intuition. My approach is centered on understanding complex
-                    problems and finding creative solutions, always looking to
-                    refine my skills and expand my perspective. I value
-                    curiosity and strive to bring a thoughtful, innovative
-                    mindset to every project I undertake.
-                  </p>
-                </div>
-              </div>
-            </div>
+      <div className={styles.content}>
+        <div className={styles.section}>
+          <div className="card bg-primary-subtle" align="center">
+            <h1>
+              <strong>Psychology Background</strong>
+            </h1>
+            <p>
+              The "Journey of Life" can be seen as a path we all walk,
+              filled with different experiences and challenges. Just like
+              moving through various landscapes—a desert, an ocean, or a
+              dense jungle—each part of life brings its own difficulties and
+              rewards.
+            </p>
+            <p>
+              At times, we might feel lost or uncertain, like wandering
+              through a desert. Other times, we might feel overwhelmed, like
+              being caught in stormy seas. We face obstacles, make mistakes,
+              and sometimes get stuck in difficult situations. But each
+              experience, whether easy or tough, helps us learn and grow.
+            </p>
+            <p>
+              Ultimately, life is about finding our way, learning from our
+              experiences, and continuing to move forward with hope, no
+              matter what challenges come our way.
+            </p>
+            <img src="public/journey.jpg" alt="The journey of life" />
+          </div>
+        </div>
+        <div className={styles.section}>
+          <div className="card bg-primary-subtle" align="center">
+            <h1>
+              <strong>Jose Muchati</strong>
+            </h1>
+            <img
+              src="https://avatars.githubusercontent.com/u/133266199?v=4"
+              alt="me"
+            />
+            <p>
+              I am driven by a passion for personal growth and continuous
+              learning, embracing challenges with adaptability and
+              intuition. My approach is centered on understanding complex
+              problems and finding creative solutions, always looking to
+              refine my skills and expand my perspective. I value
+              curiosity and strive to bring a thoughtful, innovative
+              mindset to every project I undertake.
+            </p>
           </div>
         </div>
       </div>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,86 +1,90 @@
+import styles from "../css/style.module.css";
+
 export default function Home() {
   return (
-    <div class="card" style={{ marginTop: "30px"}}>
+    <div class="card" style={{ marginTop: "30px" }}>
       <h1 align="center" class="card bg-primary-subtle">
         Jose Muchati's Deployed Projects
       </h1>
 
-      <div
-        style={{
-          margin: "10px",
-          width: "100%",
-          height: "auto",
-          display: "flex",
-          justifyContent: "space-evenly",
-        }}
-      >
+      <div className={styles.main}>
         <div
-          class="card bg-primary-subtle"
+          className="card bg-primary-subtle"
           align="center"
           style={{ marginTop: "10px", width: "20%", height: "auto" }}
         >
           <a href="https://uwttn.github.io/code-refactor/">
             <strong>code-refactor</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/code-refactor/main/assets/images/website-view.png" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/code-refactor/main/assets/images/website-view.png"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/code-refactor">Repository</a>
         </div>
 
         <div
-          class="card bg-primary-subtle"
+          className="card bg-primary-subtle"
           align="center"
           style={{ marginTop: "10px", width: "20%", height: "auto" }}
         >
           <a href="https://uwttn.github.io/portfolio/">
             <strong>portfolio</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/portfolio/main/assets/portfolio.png" alt="portfolio" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/portfolio/main/assets/portfolio.png"
+            alt="portfolio"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/portfolio">Repository</a>
         </div>
 
         <div
-          class="card bg-primary-subtle"
+          className="card bg-primary-subtle"
           align="center"
           style={{ marginTop: "10px", width: "20%", height: "auto" }}
         >
           <a href="https://uwttn.github.io/payroll/">
             <strong>payroll</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/payroll/main/assets/image.png" alt=" Portfolio" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/payroll/main/assets/image.png"
+            alt="Portfolio"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/payroll">Repository</a>
         </div>
 
         <div
+          className="card bg-primary-subtle"
           align="center"
-          class="card bg-primary-subtle"
           style={{ marginTop: "20px", width: "20%", height: "auto" }}
         >
           <a href="https://uwttn.github.io/blog/">
             <strong>blog</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/blog/main/assets/images/blog.png" alt="My blog" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/blog/main/assets/images/blog.png"
+            alt="My blog"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/blog">Repository</a>
         </div>
       </div>
 
-      <div
-        style={{
-          margin: "10px",
-          width: "100%",
-          height: "auto",
-          display: "flex",
-          justifyContent: "space-evenly"
-        }}
-      >
+      <div className={styles.main}>
         <div
-          class="card bg-primary-subtle"
+          className="card bg-primary-subtle"
           align="center"
           style={{ marginTop: "10px", width: "20%", height: "auto" }}
         >
           <a href="https://uwttn.github.io/tasks/">
             <strong>tasks</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/tasks/main/assets/images/tasks.png" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/tasks/main/assets/images/tasks.png"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/tasks">Repository</a>
         </div>
 
@@ -92,7 +96,11 @@ export default function Home() {
           <a href="https://uwttn.github.io/weather/">
             <strong>weather</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/weather/main/assets/weather.png" alt="weather" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/weather/main/assets/weather.png"
+            alt="weather"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/weather">Repository</a>
         </div>
 
@@ -104,7 +112,11 @@ export default function Home() {
           <a href="https://www.youtube.com/watch?v=BEzvjQ_8fgY&list=PLk8XMm2RW3E87aiavU5ivWAPg0ldUqFru&index=5">
             <strong>readme-generator</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/readme-generator/main/assets/readme.png" alt=" Portfolio" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/readme-generator/main/assets/readme.png"
+            alt=" Portfolio"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/readme-generator">Repository</a>
         </div>
 
@@ -116,29 +128,28 @@ export default function Home() {
           <a href="https://www.youtube.com/watch?v=fIofu08UHdI&list=PLk8XMm2RW3E87aiavU5ivWAPg0ldUqFru&index=4">
             <strong>logo-maker</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/logo-maker/main/assets/logo.png" alt="logo-maker" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/logo-maker/main/assets/logo.png"
+            alt="logo-maker"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/logo-maker">Repository</a>
         </div>
       </div>
 
-      <div
-        style={{
-          margin: "10px",
-          width: "100%",
-          height: "auto",
-          display: "flex",
-          justifyContent: "space-evenly"
-        }}
-      >
+      <div className={styles.main}>
         <div
-          class="card bg-primary-subtle"
+          className="card bg-primary-subtle"
           align="center"
           style={{ marginTop: "10px", width: "20%", height: "auto" }}
         >
           <a href="https://note-taker-xa68.onrender.com/">
             <strong>note-taker</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/note-taker/main/note.png" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/note-taker/main/note.png"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/note-taker">Repository</a>
         </div>
 
@@ -150,7 +161,11 @@ export default function Home() {
           <a href="https://www.youtube.com/watch?v=-zgpXxu2_5c&list=PLk8XMm2RW3E87aiavU5ivWAPg0ldUqFru&index=3">
             <strong>employee-tracker</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/employee-tracker/main/employees.png" alt="weather" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/employee-tracker/main/employees.png"
+            alt="weather"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/employee-tracker">Repository</a>
         </div>
 
@@ -162,7 +177,11 @@ export default function Home() {
           <a href="https://www.youtube.com/watch?v=8qXJz0RQ2d0&list=PLk8XMm2RW3E87aiavU5ivWAPg0ldUqFru&index=2">
             <strong>e-commerce</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/e-commerce/main/commerce.png" alt="e-commerce" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/e-commerce/main/commerce.png"
+            alt="e-commerce"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/e-commerce">Repository</a>
         </div>
 
@@ -174,29 +193,28 @@ export default function Home() {
           <a href="https://github.com/Uwttn/regex-tutorial">
             <strong>regex-tutorial</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/regex-tutorial/main/regex.png" alt="regex-tutorial" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/regex-tutorial/main/regex.png"
+            alt="regex-tutorial"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/regex-tutorial">Repository</a>
         </div>
       </div>
 
-      <div
-        style={{
-          margin: "10px",
-          width: "100%",
-          height: "auto",
-          display: "flex",
-          justifyContent: "space-evenly"
-        }}
-      >
+      <div className={styles.main}>
         <div
-          class="card bg-primary-subtle"
+          className="card bg-primary-subtle"
           align="center"
           style={{ marginTop: "10px", width: "20%", height: "auto" }}
         >
           <a href="https://www.youtube.com/watch?v=j8khovpKqyU">
             <strong>social-networking-API</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/social-network-API/main/network.png" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/social-network-API/main/network.png"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/social-network-API">Repository</a>
         </div>
 
@@ -208,7 +226,11 @@ export default function Home() {
           <a href="https://code-editor-q9t4.onrender.com/">
             <strong>code-editor</strong>
           </a>
-          <img src="https://raw.githubusercontent.com/Uwttn/code-editor/main/editor.png" alt="code-editor" style={{height: '100%'}}/>
+          <img
+            src="https://raw.githubusercontent.com/Uwttn/code-editor/main/editor.png"
+            alt="code-editor"
+            style={{ height: "100%" }}
+          />
           <a href="https://github.com/Uwttn/code-editor">Repository</a>
         </div>
 
@@ -234,10 +256,13 @@ export default function Home() {
           <img src="" alt="" />
         </div>
       </div>
-      <div class="card" style={{ marginTop: "30px", width: "100%", height: "auto" }}>
-      <h1 align="center" class="card bg-primary-subtle">
-       Multiple pages coming soon...
-      </h1>
+      <div
+        class="card"
+        style={{ marginTop: "30px", width: "100%", height: "auto" }}
+      >
+        <h1 align="center" class="card bg-primary-subtle">
+          Multiple pages coming soon...
+        </h1>
       </div>
     </div>
   );
