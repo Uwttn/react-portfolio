0671174fc36d3a8db94c61d780037dc82b781a52
jm <133266199+Uwttn@users.noreply.github.com>
2024-09-15 14:00:01 -0400

    Added consistency among highlights

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -17,13 +17,13 @@ export default function Home() {
           style={{ marginTop: "10px", width: "20%", height: "auto" }}
         >
           <a href="https://uwttn.github.io/portfolio/">
-            <strong>Portfolio</strong>
+            <strong className={styles.links}>Portfolio</strong>
           </a>
           <img
             src="https://github.com/Uwttn/portfolio/blob/main/assets/website-view.png?raw=true"
             style={{ height: "100%" }}
           />
-          <a href="https://github.com/Uwttn/portfolio"><strong>Repository</strong></a>
+          <a href="https://github.com/Uwttn/portfolio"><strong className={styles.links}>Repository</strong></a>
         </div>
         {/* Payroll */}
         <div
@@ -32,13 +32,13 @@ export default function Home() {
           style={{ marginTop: "10px", width: "20%", height: "auto" }}
         >
           <a href="https://uwttn.github.io/payroll/">
-            <strong>Payroll</strong>
+            <strong className={styles.links}>Payroll</strong>
           </a>
           <img
             src="https://raw.githubusercontent.com/Uwttn/payroll/main/assets/image.png"
             style={{ height: "100%" }}
           />
-          <a href="https://github.com/Uwttn/payroll"><strong>Repository</strong></a>
+          <a href="https://github.com/Uwttn/payroll"><strong className={styles.links}>Repository</strong></a>
         </div>
         {/* weather */}
         <div
@@ -47,14 +47,14 @@ export default function Home() {
           style={{ marginTop: "10px", width: "20%", height: "auto" }}
         >
           <a href="https://uwttn.github.io/weather/">
-            <strong>Weather</strong>
+            <strong className={styles.links}>Weather</strong>
           </a>
           <img
             src="https://raw.githubusercontent.com/Uwttn/weather/main/assets/weather.png"
             alt="Portfolio"
             style={{ height: "100%" }}
           />
-          <a href="https://github.com/Uwttn/weather"><strong>Repository</strong></a>
+          <a href="https://github.com/Uwttn/weather"><strong className={styles.links}>Repository</strong></a>
         </div>
         {/* FlipIt */}
         <div
@@ -63,14 +63,14 @@ export default function Home() {
           style={{ marginTop: "10px", width: "20%", height: "auto" }}
         >
           <a href="https://flipit-57ng.onrender.com/">
-            <strong>FlipIt</strong>
+            <strong className={styles.links}>FlipIt</strong>
           </a>
           <img
             src="https://github.com/user-attachments/assets/2ab1a638-e80f-4764-8242-1ca91d5e061a"
             alt="Portfolio"
             style={{ height: "100%" }}
           />
-          <a href="https://github.com/AllredW/FlipIt"><strong>Repository</strong></a>
+          <a href="https://github.com/AllredW/FlipIt"><strong className={styles.links}>Repository</strong></a>
         </div>
 
         {/* End of Container */}
