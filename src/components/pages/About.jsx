819ffe05702cb93cd730b2c1c5a80904cce8918e
jm <133266199+Uwttn@users.noreply.github.com>
2024-09-17 11:14:58 -0400

    Rewriting a better About Me

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -32,8 +32,8 @@ const MyFlip = () => {
               </a>
               <br />
               <strong>Actively seeking opportunities.</strong>
-            <MySafari />
             </div>
+            <MySafari />
           </div>
         </div>
       </div>
@@ -44,92 +44,6 @@ const MyFlip = () => {
 export default function About() {
   return (
     <>
-      {/* Education */}
-      <div className="content">
-        <div className="section">
-          <h1 className="bg-primary-subtle">Education</h1>
-          <ul className="card bg-primary-subtle">
-            <strong>University of Central Florida</strong>
-            <em>Coding Bootcamp Certification - June 2024 - September 2024</em>
-            <br />
-            <p>
-              Topics covered: JavaScript, Cascadian Style Sheet (CSS), HTML5,
-              SQL, NoSQL, GitHub, MongoDB, MySQL, PostgreSQL, Express, React,
-              Node, Handlebars, jQuery, Bootstrap, and PureCSS, C (Programming
-              Language), Git, Application Programming Interface (API),
-              Object-Oriented Programming (OOP), Computer Science Concepts,
-              MERN, REST APIs.
-            </p>
-            <div>
-              <strong>Florida Gulf Coast University</strong>
-              <div>
-                <em>BA in Psychology - August 2020 - December 2022</em>
-              </div>
-              <strong>Cumulative GPA: 3.5 - Cum Laude</strong>
-            </div>
-            <br />
-            <div>
-              <strong>Immokalee Technical College</strong>
-              <br />
-              <em>Certificate of Completion January 2017 - June 2018</em>
-            </div>
-            <br />
-            <div>
-              <strong>Marjory Stoneman Douglas</strong>
-              <br />
-              <em>High School Diploma - August 2008 - April 2012</em>
-            </div>
-          </ul>
-        </div>
-
-        {/* Employment */}
-        <div className="section">
-          <h1 className="bg-primary-subtle">Employment</h1>
-          <ul className="card bg-primary-subtle" align="left">
-            <strong>Collier County Government – Facilities Management</strong>
-            <em>Operations Coordinator – August 2023 – May 2024</em>
-            <ul>
-              <li>
-                Developed and implemented Excel formulas for efficient contract
-                management, enhancing data accuracy and automation.
-              </li>
-              <li>
-                Managed comprehensive data analysis to be utilized for
-                forecasting and strategic planning.
-              </li>
-              <li>
-                Created and maintained dashboards and pivot tables in Excel,
-                while improving data presentation and decision-making processes.
-              </li>
-              <li>
-                Used ArcGIS’s data analysis tools to improve geographical data
-                accuracy and utility for operational purposes.
-              </li>
-              <li>
-                Designed a user-friendly GIS application for management and
-                reporting of geographical data.
-              </li>
-            </ul>
-
-            <strong>Employment Network of Southwest Florida</strong>
-            <em>Administrative Assistant – March 2023 - June 2023</em>
-            <ul>
-              <li>
-                Implemented and managed billing procedure, ensuring accuracy and
-                compliance for Social Security purposes.
-              </li>
-              <li>
-                Audited client wages and managed data entry, while enhancing
-                record-keeping accuracy.
-              </li>
-              <li>
-                Developed and utilized efficient filing systems, while improving
-                document retrieval and office workflow.
-              </li>
-            </ul>
-          </ul>
-        </div>
-
         <div>
           <div
             className="section"
@@ -142,7 +56,6 @@ export default function About() {
             </a>
           </div>
         </div>
-      </div>
     </>
   );
 }
