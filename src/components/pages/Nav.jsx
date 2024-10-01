54d753690a62dcbdee3b70d5916abaead07e686e
jm <133266199+Uwttn@users.noreply.github.com>
2024-10-01 11:19:38 -0400

    Combining portfolios

---
+++ src/components/pages/Nav.jsx
@@ -0,0 +1,13 @@
+import '../css/nav.css'
+
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+<a id="home" class="navTab"><li class="navLi">Home</li></a>
+<a id="bio" class="navTab"><li class="navLi">Bio</li></a>
+<a id="projects" class="navTab"><li class="navLi">Projects</li></a>
+</ul>
+</nav>
+    );
+}
--- src/components/portfolioContainer.jsx
+++ src/components/portfolioContainer.jsx
@@ -1,9 +1,9 @@
-import Home from './pages/Home';
+import Projects from './pages/Projects';
 
 export default function PortfolioContainer() {
     return (
         <div>
-        <Home />
+        <Projects />
         </div>
     );
 }
